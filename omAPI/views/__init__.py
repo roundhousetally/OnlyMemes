339aa747c1042db44f7f301bb6fddcb3c5ea3d87
lancewburklin <1677@holbertonschool.com>
2020-10-15 15:07:45 -0500

    Setting up the API

--- omAPI/views/__init__.py
+++ omAPI/views/__init__.py
@@ -3,3 +3,5 @@
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api')
+
+from omAPI.views.test_api import posts
--- omAPI/views/test_api.py
+++ omAPI/views/test_api.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ Testing the API stuff IDK I'm not paid enough for this """
-from api import app_views
+from omAPI.views import app_views
 from flask import abort, jsonify
-from flask.globals import requests
 
 
 settings = {
@@ -10,8 +9,7 @@ settings = {
     'methods': ['GET']
 }
 
-@app_views.route('/posts' **settings)
+@app_views.route('/posts', **settings)
 def posts(profile_id=None):
-    if profile_id = None:
+    if profile_id == None:
         print("Profile ID expected")
-    
