78a4a2285c62b7917ae9c4f0b914315be0325a40
Sean Taylor <silverwolf4350@gmail.com>
2020-10-19 23:33:34 +0000

    Added the post function to Profile.

--- classes/profile.py
+++ classes/profile.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Contains the Profile class """
 from classes.base import Base, Parent
+from classes.post import Post
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+import requests
 
 class Profile(Parent, Base):
     """ Definition of a profile on the website. """
@@ -10,8 +12,25 @@ class Profile(Parent, Base):
     name = Column(String(20), unique=True, nullable=False)
     description = Column(String(240), nullable=True)
     api = Column(String(100), nullable=False)
-    pfp = Column(String(25), nullable=True)
+    pfp = Column(String(50), nullable=True)
     posts = relationship("Posts", backref="profile")
+
+    def post(self):
+        """ Tells the profile to generate a new post. """
+        p = Post()
+        r = requests.get(api).json()
+        if isinstance(r, list):
+            r = r[0]
+        if 'value' in r:
+            p.text = r['value']
+        elif 'slip' in r:
+            p.text = r['slip']['advice']
+        elif 'url' in r:
+            p.media = r['url']
+        elif 'data' in r:
+            p.media = r['data']['memes'][0]['url']
+        p.profile_id = self.id
+        p.save()
 
     def __str__(self):
         """ Returns a string representation of the instance. """
