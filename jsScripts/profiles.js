c00d930b0c012de3ec96ad507e5db5c55445cbe6
root <root@madmansilver.com>
2020-10-21 08:03:33 +0800

    Finished ironing out seems. Website functional now.

--- jsScripts/profiles.js
+++ jsScripts/profiles.js
@@ -3,13 +3,13 @@
 function getProfile () {
     $.ajax ({
 	type: 'GET',
-	url: 'http://0.0.0.0:5000/api/profiles',
+	url: 'http://onlymemes.biz/api/profiles',
 	contentType: 'application/json',
 	dataType: 'json',
 	data: JSON.stringify({}),
 	success: function (data, status) {
 	    for (let i = 0; i < data.length; i++) {
-		let pr = '<a href="onlymemes.biz/p/${data[i].name}" class=temp></a>'
+		let pr = `<a href="/p/${data[i].name}" class=temp></a>`
 		$('.profilebox').append('<div class="profilecard cheese"></div>');
 		$('.profilebox .cheese').append(pr);
 		$('.cheese').removeClass('cheese');
