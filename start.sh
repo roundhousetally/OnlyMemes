c719e1aa04670cbb33682653eba392808cc77c57
Sean Taylor <silverwolf4350@gmail.com>
2020-10-22 05:06:24 +0000

    Updated start script to include scheduler.

--- start.sh
+++ start.sh
@@ -3,4 +3,6 @@ tmux new-session -d 'gunicorn --bind 0.0.0.0:5000 omAPI.views.app:app'
 echo "API server started..."
 tmux new-session -d 'gunicorn --bind 0.0.0.0:5001 profpage:app'
 echo "Profile server started..."
+tmux new-session -d 'python3 -m classes.scheduler'
+echo "Post scheduler started..."
 echo "Finished"
